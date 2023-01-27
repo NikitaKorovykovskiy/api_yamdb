6b8e9d889fdf902807206ddaa7a9749fe7cf2c08
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2023-01-27 22:00:39 +0700

    5

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -36,7 +36,7 @@ class User(AbstractUser):
 
     @property
     def is_admin(self):
-        return self.role == self.ADMIN
+        return self.is_staff or self.role == self.ADMIN
 
     @property
     def is_moderator(self):
